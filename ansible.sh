acf74b62b646bfd8cfd0de8dde744971884d3a7b
chaitu <chaitus.ch09@gmail.com>
2024-03-29 17:13:00 -0700

    try try

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 
-ansible-playbook -i frontend, -e ansible_user=ec2-user,ansible_password=DevOps321 -e env=DEV 07-tags.yml
+ansible-playbook -i frontend.chaitus.shop, -e ansible_user=ec2-user,ansible_password=DevOps321 -e env=DEV 07-tags.yml
